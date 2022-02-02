f064d0bf710e794d018bc95862103843f37618e6
Schijtbeer <92092576+Schijtbeer@users.noreply.github.com>
2022-02-02 21:11:01 +0100

    Turret Switches

--- Buildingseason_2022/src/main/cpp/Turret.cpp
+++ Buildingseason_2022/src/main/cpp/Turret.cpp
@@ -30,44 +30,94 @@ void Turret::Jetson_Config()
 {
 frc::SmartDashboard::PutNumber("rotation",0);
 frc::SmartDashboard::PutNumber("angle",0);
+frc::SmartDashboard::PutNumber("X",0);
+frc::SmartDashboard::PutNumber("Y",0);
+frc::SmartDashboard::PutNumber("Depth",0);
+frc::SmartDashboard::PutNumber("shooting speed",0);
 }
 
 void Turret::Jetson()
 {
 // imitating input from the Jetson
- double rotation = frc::SmartDashboard::GetNumber("rotation",0);
- double angle = frc::SmartDashboard::GetNumber("angle",0);
- Rotation(rotation);
+ rotation = frc::SmartDashboard::GetNumber("rotation",0);
+ angle = frc::SmartDashboard::GetNumber("angle",0);
+ X = frc::SmartDashboard::GetNumber("X",0);
+ Y = frc::SmartDashboard::GetNumber("Y",0);
+ Depth = frc::SmartDashboard::GetNumber("Depth",0);
+ shootSpeed = frc::SmartDashboard::GetNumber("shooting speed",0);
+
+ 
+ // Rotation calculations with X
+
+ // Angle calculations with Y
+
+ // motor calculations with the Depth
 }
 
 void Turret::Rotation(double rotation)
 {
 
- if (RotSwitch.Get() == true || RotSwitch2.Get() == true) {
+R_pidController.SetReference(rotation, rev::ControlType::kPosition);
+
+if(RotSwitchL.Get() == true)
+{
+    if(X < X_Center)
+    {
         Rotation_motor.Set(0);
     }
-    else {
-    R_pidController.SetReference(rotation, rev::ControlType::kPosition);
+          else if(X >= X_Center)
+    {
+        Rotation_motor.Set(motorSpeedR);
     }
+}
+
+else if(RotSwitchR.Get() == true)
+{
+        if(X < 300)
+    {
+        Rotation_motor.Set(motorSpeedL);
+    }
+          else if(X >= 300)
+    {
+        Rotation_motor.Set(0);
+    }
+}
 }
 
 void Turret::Angle(double angle)
 {
-frc::SmartDashboard::GetNumber("Angle Turret", SetAngle);
- if (AngleSwitch.Get() == true) {
+A_pidController.SetReference(rotation, rev::ControlType::kPosition);
+
+if(AngleSwitchD.Get() == true)
+{
+    if(Y < Y_Center)
+    {
         Angle_motor.Set(0);
     }
-    else {
-        Angle_motor.Set(SetAngle);
-        A_pidController.SetReference(angle, rev::ControlType::kPosition);
+          else if(X >= Y_Center)
+    {
+        Angle_motor.Set(motorSpeedR);
     }
+}
+
+else if(AngleSwitchU.Get() == true)
+{
+        if(Y < Y_Center)
+    {
+        Angle_motor.Set(motorSpeedL);
+    }
+          else if(Y >= Y_Center)
+    {
+        Angle_motor.Set(0);
+    }
+}
 }
 
 void Turret::Shoot()
 {
     if(Set_Shot == true){
-     Turret_HIGH.Set(ControlMode::PercentOutput, ShooterSpeed);
-     Turret_master.Set(ControlMode::PercentOutput, ShooterSpeed);
+     Turret_HIGH.Set(ControlMode::PercentOutput, shootSpeed);
+     Turret_master.Set(ControlMode::PercentOutput, shootSpeed);
      if(timer_Shooter.Get()>period_shooter){
         Set_Shot == false;
      }
@@ -91,8 +141,10 @@ void Turret::HomingRotation()
 
 }
 
-void Turret::turret_mainloop()
+void Turret::turret_Teleop(double angle, double rotation)
 {
-    
+    Turret::Jetson();
+    Turret::Rotation(rotation);
+    Turret::Angle(angle);
 
 }
