ced1d37489f717790cbf78859290d502924144ef
JoepvA <j.vanaalst@teampi.nl>
2022-02-17 16:45:20 +0100

    Siuu

--- Buildingseason_2022/src/main/cpp/Turret.cpp
+++ Buildingseason_2022/src/main/cpp/Turret.cpp
@@ -3,28 +3,28 @@
 
 
 
-Turret::Turret(frc::PS4Controller *controller)
-{ 
- turret_Joystick = controller;
- Turret_master.SetInverted(true);
- Turret_slave.SetInverted(false);
- Turret_slave.Follow(Turret_master);
-}
+// Turret::Turret(frc::PS4Controller *controller)
+// { 
+//  turret_Joystick = controller;
+//  Turret_master.SetInverted(true);
+//  Turret_slave.SetInverted(false);
+//  Turret_slave.Follow(Turret_master);
+// }
 
-void Turret::Configure_Turret_PID()
-{
-    // Rotation PID config
-    R_pidController.SetP(kP_R);
-    R_pidController.SetI(kI_R);
-    R_pidController.SetD(kD_R);
-    R_pidController.SetOutputRange(kMinOutput_R, kMaxOutput_R);
+// void Turret::Configure_Turret_PID()
+// {
+//     // Rotation PID config
+//     R_pidController.SetP(kP_R);
+//     R_pidController.SetI(kI_R);
+//     R_pidController.SetD(kD_R);
+//     R_pidController.SetOutputRange(kMinOutput_R, kMaxOutput_R);
 
-    // Rotation PID config
-    A_pidController.SetP(kP_A);
-    A_pidController.SetI(kI_A);
-    A_pidController.SetD(kD_A);
-    A_pidController.SetOutputRange(kMinOutput_A, kMaxOutput_A);
-}
+//     // Rotation PID config
+//     A_pidController.SetP(kP_A);
+//     A_pidController.SetI(kI_A);
+//     A_pidController.SetD(kD_A);
+//     A_pidController.SetOutputRange(kMinOutput_A, kMaxOutput_A);
+// }
 
 void Turret::Jetson_Config()
 {
@@ -57,92 +57,92 @@ void Turret::Jetson()
  // motor calculations with the Depth
 }
 
-void Turret::Rotation(double rotation)
-{
+// void Turret::Rotation(double rotation)
+// {
 
-R_pidController.SetReference(rotation, rev::ControlType::kPosition);
+// R_pidController.SetReference(rotation, rev::ControlType::kPosition);
 
-if(RotSwitchL.Get() == true)
-{
-    if(X < X_Center)
-    {
-        Rotation_motor.Set(0);
-    }
-          else if(X >= X_Center)
-    {
-        Rotation_motor.Set(motorSpeedR);
-    }
-}
+// if(RotSwitchL.Get() == true)
+// {
+//     if(X < X_Center)
+//     {
+//         Rotation_motor.Set(0);
+//     }
+//           else if(X >= X_Center)
+//     {
+//         Rotation_motor.Set(motorSpeedR);
+//     }
+// }
 
-else if(RotSwitchR.Get() == true)
-{
-        if(X < 300)
-    {
-        Rotation_motor.Set(motorSpeedL);
-    }
-          else if(X >= 300)
-    {
-        Rotation_motor.Set(0);
-    }
-}
-}
+// else if(RotSwitchR.Get() == true)
+// {
+//         if(X < 300)
+//     {
+//         Rotation_motor.Set(motorSpeedL);
+//     }
+//           else if(X >= 300)
+//     {
+//         Rotation_motor.Set(0);
+//     }
+// }
+// }
 
-void Turret::Angle(double angle)
-{
-A_pidController.SetReference(angle, rev::ControlType::kPosition);
+// void Turret::Angle(double angle)
+// {
+// A_pidController.SetReference(angle, rev::ControlType::kPosition);
 
-if(AngleSwitchD.Get() == true)
-{
-    if(Y < Y_Center)
-    {
-        Angle_motor.Set(0);
-    }
-          else if(X >= Y_Center)
-    {
-        Angle_motor.Set(motorSpeedR);
-    }
-}
+// if(AngleSwitchD.Get() == true)
+// {
+//     if(Y < Y_Center)
+//     {
+//         Angle_motor.Set(0);
+//     }
+//           else if(X >= Y_Center)
+//     {
+//         Angle_motor.Set(motorSpeedR);
+//     }
+// }
 
-else if(AngleSwitchU.Get() == true)
-{
-        if(Y < Y_Center)
-    {
-        Angle_motor.Set(motorSpeedL);
-    }
-          else if(Y >= Y_Center)
-    {
-        Angle_motor.Set(0);
-    }
-}
-}
+// else if(AngleSwitchU.Get() == true)
+// {
+//         if(Y < Y_Center)
+//     {
+//         Angle_motor.Set(motorSpeedL);
+//     }
+//           else if(Y >= Y_Center)
+//     {
+//         Angle_motor.Set(0);
+//     }
+// }
+// }
 
-void Turret::Shoot()
-{
-    if(Set_Shot == true){
-     Turret_HIGH.Set(ControlMode::PercentOutput, shootSpeed);
-     Turret_master.Set(ControlMode::PercentOutput, shootSpeed);
-     if(timer_Shooter.Get()>period_shooter){
-        Set_Shot == false;
-     }
-    }
-}
+// void Turret::Shoot()
+// {
+//     if(Set_Shot == true){
+//      Turret_HIGH.Set(ControlMode::PercentOutput, shootSpeed);
+//      Turret_master.Set(ControlMode::PercentOutput, shootSpeed);
+//      if(timer_Shooter.Get()>period_shooter){
+//         Set_Shot == false;
+//      }
+//     }
+// }
 
-void Turret::NoShoot()
-{
-    Turret_HIGH.Set(ControlMode::PercentOutput, 0);
-    Turret_master.Set(ControlMode::PercentOutput, 0);
-}
+// void Turret::NoShoot()
+// {
+//     Turret_HIGH.Set(ControlMode::PercentOutput, 0);
+//     Turret_master.Set(ControlMode::PercentOutput, 0);
+// }
 
 
-void Turret::HomingAngle()
-{
+// void Turret::HomingAngle()
+// {
 
-}
+// }
 
-void Turret::HomingRotation()
-{
+// void Turret::HomingRotation()
+// {
 
-}
+// }
 
 void Turret::turret_Teleop(double angle, double rotation)
 {
