24345aecfcbde1ad4224d5ca502bc614c581ef51
MansurTeampi <91979549+MansurTeampi@users.noreply.github.com>
2022-03-24 14:26:10 +0100

    comments

--- Buildingseason_2022/src/main/cpp/Shooter.cpp
+++ Buildingseason_2022/src/main/cpp/Shooter.cpp
@@ -1,9 +1,9 @@
 #include "Shooter.h"
 #include <iostream>
  
-Shooter::Shooter(frc::PS4Controller *controller)
+Shooter::Shooter(frc::PS4Controller *controller) //create shoota objekt
 {
-  // Get reference for controller
+    // Get reference for controller
     Joystick = controller;  
 
     // Setting motor settings
@@ -28,7 +28,7 @@ Shooter::Shooter(frc::PS4Controller *controller)
     // create Timer
     // timer = new frc::Timer();
 }
- //function for calculating the shooting, input is distance from camera, output is motorspeed
+//function for calculating the shooting, input is distance from camera, output is motorspeed
 void Shooter::shot_calculation(){
  X2 = sqrt((pow(Depth_camera,2))-(pow(Y3,2)));//x value of the depth
  X3 = X2+0.61;//x value of depth + radius of hub
@@ -139,7 +139,7 @@ void Shooter::TeleOp()
 void Shooter::AutoInit(){  //autonomous mode initialization
     AutoTimer.Reset();  //reset timer
     AutoTimer.Start();  //start timer
-    BottomL.Set(ControlMode::PercentOutput, 0.63); //Bottom R and top follow bottom L motor
+    BottomL.Set(ControlMode::PercentOutput, 0.63); //Bottom R and TopSpinner motor follow bottom L motor so all three are set to 63% of max motorspeed
     //StorageShooter.Set(ControlMode::PercentOutput, 1); // should be 1   
     std::cout << "init";
 }
@@ -149,11 +149,11 @@ void Shooter::AutoLoop(){   //function for autonomous loop
     //    BottomL.Set(ControlMode::PercentOutput, 0);
     //    StorageShooter.Set(ControlMode::PercentOutput, 0);
     //}
-    if (AutoTimer.Get() > 2_s) {                       //if timer is past 2s set storage motor on
+    if (AutoTimer.Get() > 2_s) {   //if timer is past 2s set storage motor on
         StorageShooter.Set(ControlMode::PercentOutput, 1);
         //BottomL.Set(ControlMode::PercentOutput, ShootPercentage);
     }
-    if (AutoTimer.Get() > 8_s){                        //if timer is past 8s set storage motor and bottom left motor to off
+    if (AutoTimer.Get() > 8_s){    //if timer is past 8s set storage motor and bottom left motor to off
         BottomL.Set(ControlMode::PercentOutput, 0);
         StorageShooter.Set(ControlMode::PercentOutput, 0);
     }
